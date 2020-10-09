ff0936095e3ba4a2205b7d4147cfc58c85d4df33
Wesley Aquino <lelinho93@gmail.com>
2020-10-09 11:38:11 -0300

    interpretação de codigo 2

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -2,7 +2,7 @@
 
 1. Este código converte o valor de dolar para reais. Através da função conversorDeMoeda que recebe um valor pelo prompt, retorna o valor em reais atraves da operação do return. Armazena o valor gerado na variavel meuDinheiro chamando a funçao com o valor 100. O console imprime o valor de entrada 100 multiplicado pelo valor que o usuario digitar.
 
-
+2. Função que escolhe um tipo de investimento e calcula o valor que o investidor terá após o investimento. Função recebe como entrada dois parametros (tipoDeInvestimento, valor), na sequencia avalia qual caso de investimento que pe através do switch case. Variável novoMontante que chama a função de investimento em ações e valor 150 e outra que chama a mesma função com tipo de investimento tesouro direto e valor 200. Primeiro console.log imprime o valor de 165 e o segundo console.log imprime TIPO DE INVESTIMENTO INFORMADO INCORRETO.
 
 
 
