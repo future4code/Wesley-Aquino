da802225c6dc0c34917fdea4fdf4df5002b27af7
Wesley Aquino <lelinho93@gmail.com>
2020-10-09 11:36:30 -0300

    interpretação de código 1

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,11 @@
+/* Interpretação de Código
+
+1. Este código converte o valor de dolar para reais. Através da função conversorDeMoeda que recebe um valor pelo prompt, retorna o valor em reais atraves da operação do return. Armazena o valor gerado na variavel meuDinheiro chamando a funçao com o valor 100. O console imprime o valor de entrada 100 multiplicado pelo valor que o usuario digitar.
+
+
+
+
+
+
+
+*/
