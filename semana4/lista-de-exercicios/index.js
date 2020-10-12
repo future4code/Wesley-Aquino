60c3eb373e2f892479ca18da3949b723b9139783
Wesley Aquino <lelinho93@gmail.com>
2020-10-12 20:57:45 -0300

    Exercicio 3 Funções de array

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -259,3 +259,32 @@ const parOuImpar = array.map((numero, index, array) => {
 })
 console.log(parOuImpar)   */
 
+/* 3.
+
+const pessoas = [
+	{ nome: "Paula", idade: 12, altura: 1.8},
+	{ nome: "João", idade: 20, altura: 1.3},  
+	{ nome: "Pedro", idade: 15, altura: 1.9}, 
+	{ nome: "Luciano", idade: 22, altura: 1.8}, 
+	{ nome: "Artur", idade: 10, altura: 1.2}, 
+	{ nome: "Soter", idade: 70, altura: 1.9}
+]
+
+const pessoasQuePodemAcessar = pessoas.filter ((pessoa, index, pessoas) => {
+    if(pessoa.idade >= 14 && pessoa.idade < 60 && pessoa.altura >= 1.5){
+        return pessoa
+    
+    }
+})
+
+const pessoasQueNaoPodemAcessar = pessoas.filter ((pessoa, index, pessoas) => {
+    if(pessoa.idade < 14 || pessoa.idade > 60 || pessoa.altura < 1.5){
+        return pessoa
+    }
+})
+
+console.log(pessoasQuePodemAcessar)
+console.log(pessoasQueNaoPodemAcessar)     */
+
+
+ 
