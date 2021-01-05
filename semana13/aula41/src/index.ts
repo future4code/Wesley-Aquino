aa672f392d8f4237b20cd90bafbbcff2a548344e
Wesley Aquino <lelinho93@gmail.com>
2021-01-05 17:01:31 -0300

    Exercício 1

---
+++ semana13/aula41/src/index.ts
@@ -0,0 +1,11 @@
+// 1) 
+// a) Ele não aceita porque deve se atribuir um valor de acordo com a declaração
+// b) Usando union type e declarar number | string
+// c) Tipando as variáveis como no exempo abaixo
+
+// const person: {name: string, age: number, favoriteColor: string} = {
+//     name: "wesley",
+//     age: 25,
+//     favoriteColor: "azul"
+// }
+
