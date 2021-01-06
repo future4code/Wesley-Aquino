63d07d052377512743a6ddbbf8798b413d89d4d2
Wesley Aquino <lelinho93@gmail.com>
2021-01-06 17:43:06 -0300

    Endpoint 1

--- semana13/aula42/src/index.ts
+++ semana13/aula42/src/index.ts
@@ -1,12 +1,21 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 import cors from 'cors'
+import {countries} from "./countries"
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 
+app.get("/countries/all", (req: Request, res: Response) => {
 
+    const result = countries.map(country => ({
+        id: country.id,
+        name: country.name
+    }))
+    
+    res.status(200).send(result)
+})
 
 
 
