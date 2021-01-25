5a30d6f5c176443b293e6384afba1797a480bd57
Wesley Aquino <lelinho93@gmail.com>
2021-01-25 17:03:33 -0300

    exercício 1

---
+++ semana18/aula50/src/index.md
@@ -0,0 +1,3 @@
+# Exercicio 1
+
+* a) Acredito que sim, as possibilidades são imensas devido a quandtidade de caracteres disponível.
---
+++ semana18/aula50/src/index.ts
@@ -0,0 +1,25 @@
+import knex from "knex";
+import express from "express";
+import dotenv from "dotenv";
+import { AddressInfo } from "net";
+import { generate } from "./services/generateId";
+
+console.log(generate())
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+
+
+
+
+const server = app.listen(process.env.PORT || 3000, () => {
+  if (server) {
+    const address = server.address() as AddressInfo;
+    console.log(`Server is running in http://localhost:${address.port}`);
+  } else {
+    console.error(`Failure upon starting server.`);
+  }
+});
